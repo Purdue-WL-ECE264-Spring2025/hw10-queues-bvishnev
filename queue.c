19160e29f5d84116eab78eebef49363795ae9f9d
root <root@BenVlaptop.>
2025-04-02 22:42:39 -0400

    try 3

--- queue.c
+++ queue.c
@@ -14,6 +14,10 @@ struct game_state dequeue(struct queue *q) {
 int number_of_moves(struct game_state start) {
     struct game_state solved = {.tiles = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}}};
     
+    struct queue q = {.data = NULL};
+    enqueue(&q, start);
+
+    /*
     struct list_node * node = malloc(sizeof(struct list_node));
     if(node == NULL) {
       return -1;
@@ -23,11 +27,14 @@ int number_of_moves(struct game_state start) {
     //below not working for some reason, so copied internals above
     //struct list_node * node = new_node(serialize(start));
     struct linked_list list = {.head = node};
-    struct queue q = {.data = list};
+    struct queue q = {.data = list}; 
+    */
+
     while(q.data.head != NULL) {
         struct game_state curr_state;
         curr_state = dequeue(&q);
         if(serialize(solved) == serialize(curr_state)) {
+            free_list(q.data);
             return curr_state.num_steps;
         }
         else {
@@ -45,5 +52,6 @@ int number_of_moves(struct game_state start) {
             enqueue(&q, new_state);
         }
     }
+    free_list(q.data);
     return -1;
 }
