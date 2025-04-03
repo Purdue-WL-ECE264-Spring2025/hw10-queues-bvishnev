add870f65ddd6881f4166147b2eb46d756cffc17
root <root@BenVlaptop.>
2025-04-02 22:09:37 -0400

    first try def wrong

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,91 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  struct list_node * node = malloc(sizeof(struct list_node));
+  if(node == NULL) {
+    return NULL;
+  }
+  node -> value = value;
+  node -> next = NULL;
+  return node;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node * new_head = new_node(value);
+  if(new_head != NULL) {
+    new_head -> next = list -> head;
+    list -> head = new_head;
+  }
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node * new_tail = new_node(value);
+  if(new_tail != NULL) {
+    if(list -> head == NULL) {
+      list -> head = new_tail;
+    }
+    else {
+      struct list_node * currnode = list -> head;
+      while(currnode -> next != NULL) {
+        currnode = currnode -> next;
+      }
+      currnode -> next = new_tail;
+    }
+  }
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  if(list -> head == NULL) {
+    return 0;
+  }
+  else {
+    size_t ret_val = list -> head -> value;
+    struct list_node * new_head = list -> head -> next;
+    free(list -> head);
+    list -> head = new_head;
+    return ret_val;
+  }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) { 
+  if(list -> head == NULL) {
+    return 0;
+  }
+  else {
+    struct list_node * currnode = list -> head;
+    size_t ret_val;
+    if(currnode -> next == NULL) {
+      ret_val = list -> head -> value;
+      free(list -> head);
+      list -> head = NULL;
+      return ret_val;
+    }
+    else {
+      //advance to second-to-last node
+      while(currnode -> next -> next != NULL) {
+        currnode = currnode -> next;
+      }
+      ret_val = currnode -> next -> value;
+      free(currnode -> next);
+      currnode -> next = NULL;
+      return ret_val;
+    }
+  }
+}
 
-void free_list(struct linked_list list) {}
+void free_list(struct linked_list list) {
+  if(list.head != NULL) {
+    struct list_node * currnode = list.head;
+    struct list_node * nextnode = list.head;
+    while(currnode != NULL) {
+      nextnode = nextnode -> next;
+      free(currnode);
+      currnode = nextnode;
+    }
+    list.head = NULL;
+  }
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,51 @@
 #include "queue.h"
 #include "tile_game.h"
+#include <stdlib.h>
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    struct linked_list list = q -> data;
+    struct linked_list * listptr = &list;
+    insert_at_head(listptr, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+    struct linked_list list = q -> data;
+    struct linked_list * listptr = &list;
+    struct game_state new_state = deserialize(remove_from_tail(listptr));
+    return new_state;
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+int number_of_moves(struct game_state start) {
+    struct game_state solved = {.tiles = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}}};
+    
+    struct list_node * node = malloc(sizeof(struct list_node));
+    if(node == NULL) {
+      return -1;
+    }
+    node -> value = serialize(start);
+    node -> next = NULL;
+    //below not working for some reason, so copied internals above
+    //struct list_node * node = new_node(serialize(start));
+    struct linked_list list = {.head = node};
+    struct queue q = {.data = list};
+    struct queue * qptr = &q;
+    while(q.data.head != NULL) {
+        struct game_state curr_state;
+        struct game_state * curr_state_ptr = &curr_state;
+        curr_state = dequeue(qptr);
+        if(serialize(solved) == serialize(curr_state)) {
+            return curr_state.num_steps;
+        }
+        else {
+            move_up(curr_state_ptr);
+            enqueue(qptr, curr_state);
+            move_down(curr_state_ptr);
+            enqueue(qptr, curr_state);
+            move_left(curr_state_ptr);
+            enqueue(qptr, curr_state);
+            move_right(curr_state_ptr);
+            enqueue(qptr, curr_state);
+        }
+    }
+    return -1;
+}
